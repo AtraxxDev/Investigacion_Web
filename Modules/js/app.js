032c46b96854e82bbea2c145bf2685c7eb6dfbc5
Elliot Reyes <gametraxworks@gmail.com>
2023-10-04 11:30:08 -0600

    Agregando practica 8 Modules

---
+++ Modules/js/app.js
@@ -0,0 +1,26 @@
+// Importar las clases y funciones desde los archivos JS
+import { Triangulo, Cuadrado, Pentagono } from './poligonos.js';
+import { mostrarRelojDigital } from './reloj.js';
+import { calcularFactorial } from './factorial.js';
+
+// Ejemplos de uso
+
+// Polígonos Regulares
+const triangulo = new Triangulo([3, 4, 5]);
+console.log(`Área del triángulo: ${triangulo.calcularArea()}`);
+console.log(`Perímetro del triángulo: ${triangulo.calcularPerimetro()}`);
+
+const cuadrado = new Cuadrado([4, 4, 4, 4]);
+console.log(`Área del cuadrado: ${cuadrado.calcularArea()}`);
+console.log(`Perímetro del cuadrado: ${cuadrado.calcularPerimetro()}`);
+
+const pentagono = new Pentagono([5, 5, 5, 5, 5]);
+console.log(`Área del pentágono: ${pentagono.calcularArea()}`);
+console.log(`Perímetro del pentágono: ${pentagono.calcularPerimetro()}`);
+
+// Reloj Digital
+mostrarRelojDigital();
+
+// Factorial
+const numero = 5;
+calcularFactorial(numero);
